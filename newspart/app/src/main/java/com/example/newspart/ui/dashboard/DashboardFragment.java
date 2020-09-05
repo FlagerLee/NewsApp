6bed6b3fe97487861b3dee44a0983f31dd847220
xuyi <2018010641@secoder.net>
2020-09-06 01:23:11 +0800

    tab fixed news&covid, textview in viewpager needed, else will slide slowly rather than from one tab to another

--- newspart/app/src/main/java/com/example/newspart/ui/dashboard/DashboardFragment.java
+++ newspart/app/src/main/java/com/example/newspart/ui/dashboard/DashboardFragment.java
@@ -11,41 +11,46 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.newspart.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardFragment extends Fragment {
-
-    private DashboardViewModel dashboardViewModel;
+    private TabLayout tabs;
+    private List<String> titles;
+    private List<Fragment>pages;
+    private CovidViewAdapter adapter;
+    private ViewPager viewpager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_covid, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        final TextView textView2 = root.findViewById(R.id.text_dashboard2);
-        final TextView textView3 = root.findViewById(R.id.text_dashboard3);
-        final TextView textView4 = root.findViewById(R.id.text_dashboard4);
-        final TextView textView5 = root.findViewById(R.id.text_dashboard5);
-        final TextView textView6 = root.findViewById(R.id.text_dashboard6);
-        final TextView textView7 = root.findViewById(R.id.text_dashboard7);
-        final TextView textView8 = root.findViewById(R.id.text_dashboard8);
-        final TextView textView9 = root.findViewById(R.id.text_dashboard9);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s+"\n"+s+"\n");
-                textView2.setText(s+"\n"+s+"\n");
-                textView3.setText(s+"\n"+s+"\n");
-                textView4.setText(s+"\n"+s+"\n");
-                textView5.setText(s+"\n"+s+"\n");
-                textView6.setText(s+"\n"+s+"\n");
-                textView7.setText(s+"\n"+s+"\n");
-                textView8.setText(s+"\n"+s+"\n");
-                textView9.setText(s+"\n"+s+"\n");
-            }
-        });
+        init(root);
         return root;
+    }
+
+    private void init(View root)
+    {
+        tabs=root.findViewById(R.id.tablayout_covid);
+
+        titles=new ArrayList<>();
+        titles.add("最新新闻");
+        titles.add("疫情图谱");
+        titles.add("知疫学者");
+
+        pages=new ArrayList<>();
+        pages.add(new CovidNewsFragment());
+        pages.add(new CovidPictureFragment());
+        pages.add(new CovidScientistFragment());
+
+        adapter=new CovidViewAdapter(getChildFragmentManager(),pages,titles);
+        viewpager=root.findViewById(R.id.viewpager_covid);
+
+        tabs.setupWithViewPager(viewpager);
+        viewpager.setAdapter(adapter);
     }
 }
