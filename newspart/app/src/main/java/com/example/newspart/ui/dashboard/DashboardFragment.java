377ab795c9c5bae56cd4cca6bf793bd7547d9dac
xuyi <2018010641@secoder.net>
2020-09-05 17:12:38 +0800

    srcoll view update

--- newspart/app/src/main/java/com/example/newspart/ui/dashboard/DashboardFragment.java
+++ newspart/app/src/main/java/com/example/newspart/ui/dashboard/DashboardFragment.java
@@ -24,10 +24,26 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_covid, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
+        final TextView textView2 = root.findViewById(R.id.text_dashboard2);
+        final TextView textView3 = root.findViewById(R.id.text_dashboard3);
+        final TextView textView4 = root.findViewById(R.id.text_dashboard4);
+        final TextView textView5 = root.findViewById(R.id.text_dashboard5);
+        final TextView textView6 = root.findViewById(R.id.text_dashboard6);
+        final TextView textView7 = root.findViewById(R.id.text_dashboard7);
+        final TextView textView8 = root.findViewById(R.id.text_dashboard8);
+        final TextView textView9 = root.findViewById(R.id.text_dashboard9);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                textView.setText(s+"\n"+s+"\n");
+                textView2.setText(s+"\n"+s+"\n");
+                textView3.setText(s+"\n"+s+"\n");
+                textView4.setText(s+"\n"+s+"\n");
+                textView5.setText(s+"\n"+s+"\n");
+                textView6.setText(s+"\n"+s+"\n");
+                textView7.setText(s+"\n"+s+"\n");
+                textView8.setText(s+"\n"+s+"\n");
+                textView9.setText(s+"\n"+s+"\n");
             }
         });
         return root;
