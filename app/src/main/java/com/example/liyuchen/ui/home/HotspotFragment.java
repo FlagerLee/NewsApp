017229b56144f0e18ee4bbba6326d7b926fb1bb2
xuyi <2018010641@secoder.net>
2020-09-07 01:06:04 +0800

    add progressbar

--- app/src/main/java/com/example/liyuchen/ui/home/HotspotFragment.java
+++ app/src/main/java/com/example/liyuchen/ui/home/HotspotFragment.java
@@ -38,7 +38,6 @@ public class HotspotFragment extends Fragment {
                 super.onScrolled(recyclerView, dx, dy);
                 if((dy>0&&!recyclerView.canScrollVertically(1))||(dy<0&&!recyclerView.canScrollVertically(-1)))
                     HomeFragment.showprogressbar();
-
             }
         });
         return root;
