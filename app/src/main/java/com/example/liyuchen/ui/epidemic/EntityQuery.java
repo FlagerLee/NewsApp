40ca5501260f648d16db69fdf1bd7b4331fadfbc
FlagerLee <yc-li18@mails.tsinghua.edu.cn>
2020-09-07 18:08:44 +0800

    Save and prepare to add smart fresh layout

---
+++ app/src/main/java/com/example/liyuchen/ui/epidemic/EntityQuery.java
@@ -0,0 +1,18 @@
+package com.example.liyuchen.ui.epidemic;
+
+import com.example.liyuchen.Async.AsyncFunctions;
+import com.example.liyuchen.Async.Information;
+
+public class EntityQuery {
+    public static void Search(String entityName, AsyncFinish callback) {
+        String searchURL = Information.entityQueryURL + "?entity=" + entityName;
+        AsyncFunctions.RequestTextFromURL(searchURL, "GET", 10000, (statement, msg, errMsg) -> {
+            if(!statement) {
+                //TODO: deal with connection failed
+            }
+            else {
+
+            }
+        });
+    }
+}
